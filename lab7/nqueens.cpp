3310783c6195e8e871d66634731a231052780ad1
Judah benjamin <judahjosh@gmail.com>
2024-10-14 16:25:49 -0400

    new file:   lab6/lab5.tar 	modified:   lab7/nqueens.cpp

--- lab7/nqueens.cpp
+++ lab7/nqueens.cpp
@@ -1,8 +1,42 @@
-// Lab 7:  N-queens using backtracking 
+// Lab 7:  N-queens using backtracking
+#include <iostream>
+#include <vector>
 
-int main(int argc, char *argv[]) {
+using namespace std;
 
-  return 0;
-
+void nqueens(vector<int> &board, int col, int size)
+{
 }
 
+bool isvalid(vector<int> &board, int size)
+{
+
+  return true;
+}
+
+void print(vector<int> &board)
+{
+  // int n = 4;
+  // vector <int> & board(n, -1);
+  //  cout << "what size you you want" << endl;
+  //  while (cin >> n)
+  //  {
+  // board.push_back(n);
+  //  }
+
+  for (int i = 0; i < board.size(); i++)
+  {
+    cout << board[i] << " ";
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  int n = 4;
+  cout << "what size you you want" << endl;
+  cin >> n;
+  vector<int> board(n, -1);
+  print(board);
+
+  return 0;
+}
