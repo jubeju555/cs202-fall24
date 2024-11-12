7892e4d3ef95e66a9bf7c29baead92a1d1f517ae
judah <judahjosh@gmail.com>
2024-11-12 14:03:18 -0500

    modified:   lab10/solution.cpp

--- lab10/solution.cpp
+++ lab10/solution.cpp
@@ -14,41 +14,61 @@ cppreference: push heap and pop heap definitions
 #include <algorithm>
 #include <queue>
 
-
 using namespace std;
-class solution
-{
-private:
 
-public:
-    vector<int> heap;
-    int heappop();
-    int heappush();
-};
+void percdown(vector <int> &heap){
+    // heap[0] = heap[heap.size()-1];
+    // heap.pop_back();
+
+    int i = 0;
+
+    while (i < heap.size())
+    {
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        int min = i;
+        
+        if (left < heap.size() && heap.size() < heap[min])
+        {
+            min = left; 
+        }
+        if (right > heap.size() && heap.size() > heap[min])
+        {
+            min = right;
+        } 
+        if (min == i)
+        {
+            break;
+        }
+     int temp = heap[i];
+     heap[i] = heap[min];
+     heap[min] = temp; 
+     i = min;
+    }
+}
+
+int heappop(vector <int> &heap){
+   
+make_heap(heap.begin(), heap.end());
+heap.pop_back();
+pop_heap(heap.begin(), heap.end());
+return heap.back();
+
+
+}
+
+// int solution::heappush(){
+// make_heap(heap.begin(), heap.end());
+// heap.pop_back();
+// pop_heap(heap.begin(), heap.end());
+// sort_heap(heap.begin(), heap.end());
+// return heap.back();
+
+// }
 
 int main(int argc, char *argv[]) {
-
-
-
-}
-
-int solution::heappop(){
-make_heap(heap.begin(), heap.end());
-heap.pop_back();
-pop_heap(heap.begin(), heap.end());
-return heap.back();
-
+    vector<int> heap;
 
 }
-
-int solution::heappush(){
-make_heap(heap.begin(), heap.end());
-heap.pop_back();
-pop_heap(heap.begin(), heap.end());
-sort_heap(heap.begin(), heap.end());
-return heap.back();
-
-}
-
 
 
