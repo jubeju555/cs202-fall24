78111ff3d82ae0327862b071046501f6ae57d6b8
judah <judahjosh@gmail.com>
2024-11-12 10:35:50 -0500

    new file:   lab10/.vscode/settings.json 	modified:   lab10/solution.cpp

--- lab10/solution.cpp
+++ lab10/solution.cpp
@@ -4,13 +4,37 @@
 // Brief description:
 
 // This code reads in a number of ints...
+/*
+sources: 
+cppreference: push heap and pop heap definitions 
 
+*/
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <queue>
+
 
 using namespace std;
 
+int heappop(){
+vector<int> heap;
+make_heap(heap.begin(), heap.end());
+println("after make_heap: ", heap);
+heap.pop_back();
+println("after make_heap: ", heap);
+pop_heap(heap.begin(), heap.end());
+println("after make_heap: ", heap);
+}
+
+
+
+
 int main(int argc, char *argv[]) {
+
+
+
+
 
 
 }
